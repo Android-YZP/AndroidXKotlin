5915c0641b594c4ed508a9d498879b13706fc548
Smith <724784530@qq.com>
2020-09-23 09:38:57 +0800

    Kotlin学习

--- app/src/main/java/com/yzp/androidxkotlin/MainActivity.kt
+++ app/src/main/java/com/yzp/androidxkotlin/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.yzp.androidxkotlin
 
+import android.app.ProgressDialog.show
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+
 }
