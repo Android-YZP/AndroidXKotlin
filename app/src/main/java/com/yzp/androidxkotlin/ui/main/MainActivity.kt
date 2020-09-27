36f2b3989890505826be86320c69624f9bac03b7
Smith <724784530@qq.com>
2020-09-27 18:06:30 +0800

    Kotlin学习

--- app/src/main/java/com/yzp/androidxkotlin/ui/main/MainActivity.kt
+++ app/src/main/java/com/yzp/androidxkotlin/ui/main/MainActivity.kt
@@ -22,10 +22,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initData() {
-        data.add("列表")
         data.add("携程")
         data.add("个人主页框架")
-        data.add("主界面框架")
+        data.add("轮播图")
+        data.add("列表")
+        data.add("WebView")
+        data.add("Room的使用")
+        data.add("网络请求缓存的使用")
+        data.add("Android版本适配")
+        data.add("Android权限申请")
+        data.add("文件下载/上传")
+        data.add("图片选择")
+        data.add("音频播放")
+        data.add("视频播放")
+        data.add("地图")
+        data.add("USB/蓝牙/Wifi")
+        data.add("串口通信")
 
         with(rv) {
             val linearLayoutManager = LinearLayoutManager(this@MainActivity)
