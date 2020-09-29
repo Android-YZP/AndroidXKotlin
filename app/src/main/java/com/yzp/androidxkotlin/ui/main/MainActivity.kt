f72066b5ad00c3141f6a2aeabc365461fa8795de
Smith <724784530@qq.com>
2020-09-29 15:03:57 +0800

    节目编写

--- app/src/main/java/com/yzp/androidxkotlin/ui/main/MainActivity.kt
+++ app/src/main/java/com/yzp/androidxkotlin/ui/main/MainActivity.kt
@@ -15,6 +15,7 @@ import com.yzp.androidxkotlin.R
 import com.yzp.androidxkotlin.bean.MainBean
 import com.yzp.androidxkotlin.ui.coroutines.CoroutineActivity
 import com.yzp.androidxkotlin.ui.home.HomeActivity
+import com.yzp.androidxkotlin.ui.updateapk.UpdateActivity
 import com.yzp.mvvmlibrary.base.BaseActivity
 import com.yzp.mvvmlibrary.base.NoViewModel
 import kotlinx.android.synthetic.main.activity_main.*
@@ -59,12 +60,17 @@ class MainActivity : BaseActivity<NoViewModel, ViewDataBinding>() {
     }
 
     override fun initData() {
-        data.add(MainBean("携程", CoroutineActivity::class.java))
+        data.add(MainBean("协程", CoroutineActivity::class.java))
+        data.add(MainBean("Lifecycle的使用", CoroutineActivity::class.java))
+        data.add(MainBean("LiveDataViewModel的使用", CoroutineActivity::class.java))
+        data.add(MainBean("Navigation的使用", CoroutineActivity::class.java))
+        data.add(MainBean("WorkManager的使用", CoroutineActivity::class.java))
+        data.add(MainBean("DataBinding的使用", CoroutineActivity::class.java))
+        data.add(MainBean("Room的使用", CoroutineActivity::class.java))
         data.add(MainBean("个人主页框架", CoroutineActivity::class.java))
         data.add(MainBean("列表", CoroutineActivity::class.java))
-        data.add(MainBean("WebView", CoroutineActivity::class.java))
-        data.add(MainBean("Room的使用", CoroutineActivity::class.java))
-        data.add(MainBean("网络请求缓存的使用", HomeActivity::class.java))
+        data.add(MainBean("网络请求", HomeActivity::class.java))
+        data.add(MainBean("版本更新", UpdateActivity::class.java))
         data.add(MainBean("Android版本适配", CoroutineActivity::class.java))
         data.add(MainBean("Android权限申请", CoroutineActivity::class.java))
         data.add(MainBean("文件下载/上传", CoroutineActivity::class.java))
@@ -72,6 +78,7 @@ class MainActivity : BaseActivity<NoViewModel, ViewDataBinding>() {
         data.add(MainBean("音频播放", CoroutineActivity::class.java))
         data.add(MainBean("视频播放", CoroutineActivity::class.java))
         data.add(MainBean("地图", CoroutineActivity::class.java))
+        data.add(MainBean("WebView", CoroutineActivity::class.java))
         data.add(MainBean("蓝牙", CoroutineActivity::class.java))
         data.add(MainBean("Wifi", CoroutineActivity::class.java))
         data.add(MainBean("串口通信", CoroutineActivity::class.java))
